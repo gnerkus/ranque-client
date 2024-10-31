fa9a78ce32dd6dbf91dd6a87a9fbc7c8cb3ed18c
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-31 19:46:03 +0100

    scaffold configuration file

---
+++ config.js
@@ -0,0 +1,37 @@
+﻿function load(envName) {
+  const base = require("./config/base.json");
+  const env = loadConfig(envName);
+
+  return {
+    ...base,
+    ...env,
+    dev: {
+      ...base.dev,
+      ...env.dev,
+    }
+  };
+}
+
+function log(configObj) {
+  const repeatNum = 50;
+  console.log(`${"=".repeat(repeatNum)}\nenvConfig`);
+  console.log(JSON.stringify(configObj, null, 2));
+  console.log(`${"=".repeat(repeatNum)}`);
+}
+
+function loadConfig(configName) {
+  try {
+    return require(`./config/${configName}.json`);
+  } catch (e) {
+    if (e instanceof Error && e.code === "MODULE_NOT_FOUND") {
+      return {};
+    } else {
+      throw e;
+    }
+  }
+}
+
+module.exports = {
+  load,
+  log,
+};
