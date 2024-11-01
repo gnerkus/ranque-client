284f0d7bf21cd56de097359391d8ed18473e85d3
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-01 14:53:20 +0100

    scaffold directive for user's auth state
    define module for shared objects

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+﻿import {NgModule} from "@angular/core";
+import {ShowAuthedDirective} from "./show-authed.directive";
+
+@NgModule({
+  imports: [],
+  exports: [
+    ShowAuthedDirective
+  ],
+  declarations: [ShowAuthedDirective]
+})
+
+export class SharedModule {}
