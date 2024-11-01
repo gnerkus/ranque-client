eb1ae42119d93def1346832f05f5883b944741bb
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-01 13:26:42 +0100

    redirect to dashboard for no route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent} from "./dashboard/dashboard.component";
 
+// TODO: define a PageNotFoundComponent and set it to the wildcard route
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home/dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/home/dashboard', pathMatch: 'full' },
+  { path: '**', component: DashboardComponent },
 ];
