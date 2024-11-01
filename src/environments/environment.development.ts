7b55fa30855bbea11bb201f4136021c5bb0f8579
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-01 13:26:15 +0100

    add host to env config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
+  host: 'http://localhost:4200',
   apiUrl: 'https://localhost:5001'
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
+  host: 'https://ranque-client.pages.dev/',
   apiUrl: 'https://nanotome-bookmark-api.azurewebsites.net'
 };
