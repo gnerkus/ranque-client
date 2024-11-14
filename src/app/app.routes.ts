c74233d7f2f19451cdd87634bed3499264e55378
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-14 17:09:12 +0100

    add home page
    add main navigation to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginComponent} from "./auth/login.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {RegisterComponent} from "./auth/register.component";
+import {HomeComponent} from "./home/home.component";
 
 // TODO: define a PageNotFoundComponent and set it to the wildcard route
 export const routes: Routes = [
@@ -10,7 +11,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard/home', component: DashboardComponent },
   { path: 'dashboard', redirectTo: 'dashboard/home', pathMatch: 'full' },
-  { path: '', redirectTo: 'dashboard/home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
