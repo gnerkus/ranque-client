b2dc9082b0b60721a34a62e7f37d023ab8e96594
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-14 19:56:30 +0100

    add storage service for jwt tokens to root

---
+++ src/app/shared/jwtStorage.service.ts
@@ -0,0 +1,23 @@
+﻿import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class JwtStorageService {
+  getItem(): Observable<string | null> {
+    const data = sessionStorage.getItem('token');
+    if (data) {
+      return of(data);
+    }
+    return of(null);
+  }
+
+  setItem(data: string): Observable<string> {
+    sessionStorage.setItem('token', data);
+    return of(data);
+  }
+
+  removeItem(): Observable<boolean> {
+    sessionStorage.removeItem('token');
+    return of(true);
+  }
+}
