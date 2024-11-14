f9b23cfd50e63aef4fe81e94181168afa5507d95
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-14 20:02:16 +0100

    require login to access dashboard

---
+++ src/app/auth/auth-guard.ts
@@ -0,0 +1,19 @@
+﻿import { inject } from '@angular/core';
+import { map, Observable, take } from 'rxjs';
+import { Router, UrlTree } from '@angular/router';
+import {JwtStorageService} from "../shared/jwtStorage.service";
+
+export const authGuard = (): Observable<boolean | UrlTree> => {
+  const storage = inject(JwtStorageService);
+  const router = inject(Router);
+
+  return storage.getItem().pipe(
+    map((token) => {
+      if (!token) {
+        return router.parseUrl('/login');
+      }
+      return true;
+    }),
+    take(1),
+  );
+};
